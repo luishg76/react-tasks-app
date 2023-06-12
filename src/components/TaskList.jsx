7e18a96b057ec7433630bc8fcaf1a9cae04cf927
Luis Hernández Gómez <ing.luishg76@gmail.com>
2023-06-11 22:32:46 -0500

    Para-Produccion(11-06-023)H

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,28 +3,24 @@ import { useContext } from "react";
 import { TaskContext } from "../contexts/TaskContext.jsx";
 
 function TaskList() {
-  const {taskslist}=useContext(TaskContext)
+  const { taskslist } = useContext(TaskContext);
   //estado
-  
+
   return (
     <div>
-      {taskslist.length===0?(
-        <h2 className="text-white text-3xl text-center">No existen tareas que mostrar</h2>
-    ):
-    (
-      <div className="grid grid-cols-4 gap-2"> 
-      {
-        taskslist.map((task) => {
-          return(
-            <TaskCard key={task.id} task={task}/>
-          )
-        })        
-      }          
-      </div>   
-    )
-    } 
-    </div>    
-  )
+      {taskslist.length === 0 ? (
+        <h2 className="text-white text-3xl text-center">
+          No existen tareas que mostrar
+        </h2>
+      ) : (
+        <div className="grid grid-cols-4 gap-2">
+          {taskslist.map((task) => {
+            return <TaskCard key={task.id} task={task} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TaskList;
